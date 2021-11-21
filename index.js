6a833e3858a6e584a410ae7b83c887a6be6379a8
ashadul-mridha <ashadulislam708@gmail.com>
2021-11-22 01:29:09 +0600

    remote cng

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riphr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
+async function  run(){
 
     try{
         await client.connect();
